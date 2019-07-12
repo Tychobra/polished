a56dc9e26560aadb80f9e99e42eea0959c51601a
merlinoa <andy.merlino@tychobra.com>
2019-07-12 11:00:09 -0400

    corrected system file path and added `overwrite` argument

--- R/write_firebase_functions.R
+++ R/write_firebase_functions.R
@@ -1,20 +1,23 @@
 #' write the index.js file
-#' 
-#' @param path The file path of the created file
-#' 
+#'
+#' @param path "functions/index.js" by default. The file path of the created file.
+#' @param overwrite TRUE by default.  Should the existing file
+#' be overwritted
+#'
 #' @export
-#' 
-#' @examples 
-#' 
-#' #must make functions folder
+#'
+#' @examples
+#'
+#' # must make functions folder
 #' write_firebase_functions()
-#' 
+#'
 #' write_firebase_functions("inst")
-#' 
-write_firebase_functions <- function(path = "functions/index.js") {
-  
+#'
+write_firebase_functions <- function(path = "functions/index.js", overwrite = TRUE) {
+
   file.copy(
-    from = system.file("inst/firebase_functions/index.js", package = "polished"),
-    to = path
+    from = system.file("firebase_functions/index.js", package = "polished"),
+    to = path,
+    overwrite = overwrite
   )
 }
