60111102c7260ef109e9f64cb5d2f77f6a858acc
merlinoa <andy.merlino@tychobra.com>
2019-07-15 13:57:19 -0400

    new `profile_module` for `shinydashboard`

--- R/dashboard_module.R
+++ R/dashboard_module.R
@@ -1,18 +1,18 @@
 #' dashboard_module_ui
-#' 
+#'
 #' @param id the module id
-#' 
+#'
 #' @import shiny shinydashboard apexcharter xts dplyr DT shinycssloaders
-#' 
+#'
 #' @export
 dashboard_module_ui <- function(id) {
   ns <- NS(id)
-  
+
   tabItem(
     tabName = "dashboard",
     shiny::fluidRow(
       shinydashboard::valueBox(
-        value = 6,
+        value = NA,
         subtitle = "Total Users",
         icon = icon("users"),
         color = "blue",
@@ -25,14 +25,14 @@ dashboard_module_ui <- function(id) {
         width = 3
       ),
       shinydashboard::valueBox(
-        value = 2,
+        value = NA,
         subtitle = "DAU",
         icon = icon("users"),
         color = "teal",
         width = 3
       ),
       shinydashboard::valueBox(
-        value = 4,
+        value = NA,
         subtitle = "MAU",
         icon = icon("users"),
         color = "green",
@@ -42,28 +42,29 @@ dashboard_module_ui <- function(id) {
     shiny::fluidRow(
       shinydashboard::box(
         width = 9,
+        title = "Placeholder Chart",
         apexcharter::apexchartOutput(ns("dau_chart")) %>%
           shinycssloaders::withSpinner(type = 8)
       ),
       shinydashboard::box(
         width = 3,
         title = "Active Users",
-        DT::DTOutput(ns("active_users_table")) %>% 
+        DT::DTOutput(ns("active_users_table")) %>%
           shinycssloaders::withSpinner(type = 8, proxy.height = "341.82px"),
         br()
-      ) 
+      )
     )
   )
 }
 
 #' dashboard_module
-#' 
+#'
 #' @import shiny
-#' 
+#'
 #' @export
 dashboard_module <- function(input, output, session) {
   ns <- session$ns
-  
+
   global_users_prep <- shiny::reactivePoll(
     intervalMillis = 1000 * 10, # check every 10 seconds,
     session = session,
@@ -74,12 +75,12 @@ dashboard_module <- function(input, output, session) {
       .global_users$users
     }
   )
-  
-  
+
+
   active_users_number_prep <- reactive({
     users_list <- global_users_prep()
     users <- unique(lapply(users_list, function(user) user$get_email()))
-    
+
     length(users)
   })
 
@@ -89,32 +90,32 @@ dashboard_module <- function(input, output, session) {
     active_users_number_prep,
     reactive("Active Users")
   )
-  
-  
+
+
   dau_chart_prep <- reactive({
     date_strings <- c("2019-07-01", "2019-07-02", "2019-07-03", "2019-07-04", "2019-07-05",
                       "2019-07-06", "2019-07-07", "2019-07-08", "2019-07-09", "2019-07-10",
                       "2019-07-11", "2019-07-12", "2019-07-13", "2019-07-14")
-    
+
     dates <- as.POSIXct(date_strings)
-    
+
     dplyr::tibble(
       input = c(3,2,2,3,1,4,2,5,7,6,9,15,16,16),
       date = dates
     )
   })
-  
+
   output$dau_chart <- apexcharter::renderApexchart({
     dat <- dau_chart_prep()
 
-    apexcharter::apexchart() %>% 
+    apexcharter::apexchart() %>%
       apexcharter::ax_title(
         "Daily Active Users",
         align = "center",
         style = list(
           fontSize = 18
         )
-      ) %>% 
+      ) %>%
       apexcharter::ax_chart(
         type = "area",
         zoom = list(
@@ -129,13 +130,13 @@ dashboard_module <- function(input, output, session) {
             zoomout = FALSE
           )
         )
-      ) %>% 
+      ) %>%
       apexcharter::ax_xaxis(
         type = "datetime",
         categories = dat$date
-      ) %>% 
-      apexcharter::ax_stroke(show = TRUE, curve = "straight") %>% 
-      apexcharter::ax_dataLabels(enabled = FALSE) %>% 
+      ) %>%
+      apexcharter::ax_stroke(show = TRUE, curve = "straight") %>%
+      apexcharter::ax_dataLabels(enabled = FALSE) %>%
       apexcharter::ax_fill(
         type = "gradient",
         gradient = list(
@@ -144,25 +145,25 @@ dashboard_module <- function(input, output, session) {
           opacityTo = 0.9,
           stops = list(0, 100)
         )
-      ) %>% 
+      ) %>%
       apexcharter::ax_series(list(data = dat$input, name = "DAU"))
   })
-  
-  
+
+
   active_users_prep <- reactive({
     users_list <- global_users_prep()
-    
+
     user_emails <- unlist(lapply(users_list, function(user) user$get_email()))
-    
+
     dplyr::tibble(
       email = unique(user_emails),
       time = "13:09:00"
     )
   })
-  
+
   output$active_users_table <- DT::renderDataTable({
     out <- active_users_prep()
-    
+
     DT::datatable(
       out,
       rownames = FALSE,
---
+++ R/profile_module.R
@@ -0,0 +1,62 @@
+#' verify email page ui
+#'
+#' @param firebase_config Firebase configuration
+#'
+#' @importFrom htmltools tags
+#' @importFrom shiny textOutput actionLink
+#'
+#' @export
+profile_module_ui <- function(id, firebase_config) {
+  ns <- NS(id)
+
+  htmltools::tags$li(
+    class = "dropdown",
+    htmltools::tags$a(
+      href="#",
+      class = "dropdown-toggle",
+      `data-toggle` = "dropdown",
+      htmltools::tags$i(
+        class = "fa fa-user"
+      )
+    ),
+    htmltools::tags$ul(
+      class = "dropdown-menu",
+      htmltools::tags$li(
+        shiny::textOutput(ns("auth_user")),
+        style='padding: 3px 20px;'
+      ),
+      htmltools::tags$li(
+        shiny::actionLink(
+          "polish__sign_out",
+          label = "Sign Out",
+          icon = icon("sign-out")
+        )
+      )
+    )
+  )
+}
+
+#' profile module server
+#'
+#' @export
+#'
+#' @importFrom shiny renderText observeEvent
+#'
+#'
+profile_module <- function(input, output, session) {
+
+  output$auth_user <- shiny::renderText({
+    req(session$userData$current_user())
+
+    session$userData$current_user()$email
+  })
+
+  shiny::observeEvent(input$polish__sign_out, {
+    req(session$userData$current_user()$email)
+
+    sign_out_from_shiny(session, session$userData$current_user()$uid)
+  })
+
+}
+
+
