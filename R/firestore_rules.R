716f2fc0f2fb5cb8cabfd486ff6da41a10014c8f
merlinoa <andy.merlino@tychobra.com>
2019-07-18 06:50:44 -0400

    added "sessions" rule to firestore.rules

--- R/firestore_rules.R
+++ R/firestore_rules.R
@@ -1,17 +1,17 @@
 #' generate firestore rules text
-#' 
+#'
 #' This text will be used to write the firestore.rules file
-#' 
+#'
 #' @param app_names the names of all apps using firebase hosting
-#' 
+#'
 #' @export
 #'
-#' @example 
-#' 
+#' @example
+#'
 #' generate_firestore_rules_text("shiny_1")
-#' 
+#'
 #' generate_firestore_rules_text("shiny_1, shiny_2")
-#' 
+#'
 generate_firestore_rules_text <- function(app_names) {
   matches <- unlist(lapply(app_names, function(name) {
     c(
@@ -26,13 +26,19 @@ generate_firestore_rules_text <- function(app_names) {
       "",
       "      allow read: if request.auth.uid != null;",
       "",
-      paste0('      allow write: if request.auth.uid != null && is_admin(database, request.auth.token.email, "', name, '")'),
+      paste0('      allow write: if request.auth.uid != null && is_admin(database, request.auth.token.email, "', name, '");'),
+      "    }",
       "",
+      paste0("    match /apps/", name, "/sessions/{session} {"),
+      "",
+      paste0("      allow read: if is_admin(database, request.auth.token.email, ", name, ");"),
+      "",
+      "      allow write: if false;",
       "    }",
       ""
     )
   }))
-  
+
   c(
     "rules_version = '2';",
     "service cloud.firestore {",
@@ -56,22 +62,22 @@ generate_firestore_rules_text <- function(app_names) {
 #' @param app_names The name(s) of the apps used
 #' @param dir The directory for firestore.rules
 #' @param file_name The name of the new file
-#' 
+#'
 #' @export
-#' 
-#' @examples 
-#' 
+#'
+#' @examples
+#'
 #' write_firestore_rules("project1")
-#' 
+#'
 #' write_firestore_rules(c("project1", "project2"), "R", "file.rules")
 #'
 write_firestore_rules <- function(app_names, dir = ".", file_name = "firestore.rules") {
-  
+
   rules_text <- generate_firestore_rules_text(app_names)
-  
+
   file_conn <- file(file.path(dir, file_name))
   on.exit(close(file_conn), add = TRUE)
-  
+
   writeLines(
     rules_text,
     file_conn
