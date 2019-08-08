a4d9df1b31cd1fdd9f18311b434c7ae436db7683
merlinoa <andy.merlino@tychobra.com>
2019-08-08 18:17:48 -0400

    removed unneeded if statement

--- R/set_config_env.R
+++ R/set_config_env.R
@@ -1,16 +1,16 @@
 #' set_config_env
-#' 
+#'
 #' Determines if the app is deployed or running locally and adjusts the
 #' config environment accordingly
 #'
-#' @param override Set the environment to "default" or "production" instead of 
-#' the value found through session$clientData$hostname
-#' 
+#' @param override Set the environment to "default" or "production" instead of
+#' the value found through session$clientData$url_hostname
+#'
 #' @export
 #'
 set_config_env <- function(session, override = NULL) {
   stopifnot(is.null(override) || override %in% c("default", "production"))
-  
+
   if (is.null(override)) {
     if (isTRUE(session$clientData$url_hostname == "127.0.0.1")) {
       environment <- "default"
@@ -20,6 +20,6 @@ set_config_env <- function(session, override = NULL) {
   } else {
     environment <- override
   }
-  
-  if (!is.null(environment)) Sys.setenv(R_CONFIG_ACTIVE = environment)
+
+  Sys.setenv(R_CONFIG_ACTIVE = environment)
 }
