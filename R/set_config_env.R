26cf65c277658dcd5d3f64a95f5781c7b4d5502d
phoward38 <phoward38@gatech.edu>
2019-08-14 16:55:04 -0400

    Removed 'session' from function since it can't be read from 'global.R'

--- R/set_config_env.R
+++ R/set_config_env.R
@@ -1,21 +1,21 @@
 #' set_config_env
 #'
-#' Determines if the app is deployed or running locally and adjusts the
-#' config environment accordingly
+#' Determines if the app is deployed on shinyapps.io, where the environment variable USER is 'shiny', 
+#' or running locally and adjusts the config environment accordingly
 #'
 #' @param override Set the environment to "default" or "production" instead of
 #' the value found through session$clientData$url_hostname
 #'
 #' @export
 #'
-set_config_env <- function(session, override = NULL) {
+set_config_env <- function(override = NULL) {
   stopifnot(is.null(override) || override %in% c("default", "production"))
 
   if (is.null(override)) {
-    if (isTRUE(session$clientData$url_hostname == "127.0.0.1")) {
-      environment <- "default"
-    } else {
+    if (isTRUE(Sys.getenv('USER') == "shiny")) {
       environment <- "production"
+    } else {
+      environment <- "default"
     }
   } else {
     environment <- override
