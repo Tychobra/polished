9172dcaba0c3b634e063af35e7f029c77789f71b
merlinoa <andy.merlino@tychobra.com>
2019-11-06 12:58:38 -0500

    vectorized `create_uid()`

--- R/create_uid.R
+++ R/create_uid.R
@@ -2,12 +2,36 @@
 #'
 #' user for the uids in the database
 #'
+#' @param n length 1 integer > 0.  The number of uids to create.
+#'
 #' @return a character string uid
 #'
-#' @importFrom digest digest
 #' @importFrom stats runif
 #'
-create_uid <- function() {
+#' @export
+#'
+#' @examples
+#'
+#' # create a single uid
+#' create_uid()
+#'
+#' # create 10 uids
+#' create_uid(10)
+#'
+create_uid <- function(n = 1) {
+  stopifnot(length(n) == 1)
+  stopifnot(n > 0)
 
-  paste0("p", digest::digest(c(runif(1), Sys.time())))
+
+  paste0(
+    "p",
+    vdigest(runif(n))
+  )
 }
+
+#' vectorize the
+#'
+#' @importFrom digest digest
+#'
+#' @noRd
+vdigest <- Vectorize(digest::digest)
