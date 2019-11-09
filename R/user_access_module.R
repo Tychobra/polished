67987d4daec1cd77f3c6702c56edee0a0c311e01
merlinoa <andy.merlino@tychobra.com>
2019-11-09 16:12:14 -0500

    users table - don't try to format empty date

--- R/user_access_module.R
+++ R/user_access_module.R
@@ -242,6 +242,10 @@ user_access_module <- function(input, output, session) {
         ),
         rowCallback = JS(
           "function(row, data) {",
+
+            "var date_string = data[5]",
+            "if (date_string === null) return",
+
             "// convert time last signed in to time if time last signed in is today",
             "// or to date if time last signed in is a date prior to today",
             "function is_today(date) {
@@ -250,8 +254,6 @@ user_access_module <- function(input, output, session) {
               date.getMonth() == today.getMonth() &&
               date.getFullYear() == today.getFullYear()
             }",
-
-            "var date_string = data[5]",
 
             "// convert date string to a JS date",
             "var date_local = new Date(date_string)",
