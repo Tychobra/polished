b114065115320ac812b95ee93c9a436a850ea1cb
merlinoa <andy.merlino@tychobra.com>
2019-12-02 15:16:04 -0500

    `get_last_active_session_time()` function

---
+++ R/get_last_active_session_time.R
@@ -0,0 +1,36 @@
+#' get last active session time
+#'
+#' find the time that the last session became active for all users of a particular app
+#'
+#' @param conn the database connection
+#'
+#' @importFrom dplyr tbl filter collect group_by summarize ungroup
+#' @importFrom dbplyr in_schema
+#'
+#' @export
+#'
+#'
+#'
+get_last_active_session_time <- function(conn, app_name_) {
+
+  last_user_app_sessions <- conn %>%
+    dplyr::tbl(dbplyr::in_schema('polished', 'sessions')) %>%
+    dplyr::filter(.data$app_name == app_name_) %>%
+    dplyr::collect() %>%
+    dplyr::group_by(.data$user_uid) %>%
+    dplyr::filter(created_at == max(.data$created_at, na.rm = TRUE)) %>%
+    dplyr::ungroup()
+
+  session_uids <- last_user_app_sessions$uid
+
+  conn %>%
+    dplyr::tbl(dbplyr::in_schema('polished', 'session_actions')) %>%
+    dplyr::filter(
+      .data$session_uid %in% session_uids,
+      .data$action == 'activate'
+    ) %>%
+    dplyr::group_by(.data$session_uid) %>%
+    dplyr::filter(timestamp == max(.data$timestamp, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::collect()
+}
