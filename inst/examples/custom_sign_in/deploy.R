103f80cd3aab0786e5374963773163e669626ae8
merlinoa <andy.merlino@tychobra.com>
2020-03-07 18:07:43 -0500

    simplified file structure for example Shiny apps

--- inst/examples/custom_sign_in/deploy.R
+++
@@ -1,7 +0,0 @@
-
-app_name <- config::get(file = "shiny_app/config.yml")$app_name
-rsconnect::deployApp(
-  appDir = "shiny_app",
-  account = "tychobra",
-  appName = app_name
-)
---
+++ inst/examples/polished_setup.R
@@ -0,0 +1,32 @@
+library(polished)
+
+db_config <- config::get(file = "polished_example_01/config.yml")$db
+
+# connect to your PostgreSQL database
+db_conn <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  dbname = db_config$dbname,
+  host = db_config$host,
+  user = db_config$user,
+  password = db_config$password
+)
+
+# polished::create_schema(db_conn)
+
+# add the first user to your first app
+# I always add myself first using this function, and then I add other additional users via
+# the "Polished Admin > User Access" page
+
+polished::create_app_user(
+  db_conn,
+  app_name = "polished_example_01",
+  email = "andy.merlino@tychobra.com",
+  is_admin = TRUE
+)
+
+polished::create_app_user(
+  db_conn,
+  app_name = "custom_sign_in",
+  email = "andy.merlino@tychobra.com",
+  is_admin = TRUE
+)
