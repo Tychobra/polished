e7799c838d149064739974130a837d733fb69004
merlinoa <andy.merlino@tychobra.com>
2020-03-10 10:06:25 -0400

    removed `user` argument from `sign_out_from_shiny()`

--- R/sign_out_from_shiny.R
+++ R/sign_out_from_shiny.R
@@ -1,19 +1,10 @@
 #' sign_out_from_shiny
 #'
 #' @param session the Shiny session
-#' @param user optional user list.  A list containing the following elements
-#' - user_uid
-#' - session_uid
-#' This argument is used when we need to sign the user out of polished before the
-#' `session$userData$user()` reactive has been set.  e.g. we call
-#' `sign_out_from_shiny(session, user = list(user_uid = <user's uid>, session_uid = <session uid>))`
-#' to sign the user out of polished if there is an error in the email verification process.  If the
-#' user is already signed into the app (i.e. the `session$userData$user()` is set), then this argument
-#' should be NULL and we can grab the user_uid and session_uid from the `session$userData$user()` object.
 #'
 #' @export
 #'
-sign_out_from_shiny <- function(session, user = NULL) {
+sign_out_from_shiny <- function(session) {
 
   if (is.null(user)) {
     user <- session$userData$user()
