65af233738ee6fe52079c395b47b9deb8b98ae75
merlinoa <andy.merlino@tychobra.com>
2020-03-29 13:17:24 -0400

    added sign out button to minimal example Shiny app

--- inst/examples/polished_example_01/ui.R
+++ inst/examples/polished_example_01/ui.R
@@ -3,9 +3,19 @@
 ui <- fluidPage(
   fluidRow(
     column(
-      12,
+      6,
       h1("Polished Example 01"),
       br()
+    ),
+    column(
+      6,
+      br(),
+      actionButton(
+        "sign_out",
+        "Sign Out",
+        icon = icon("sign-out-alt"),
+        class = "pull-right"
+      )
     ),
     column(
       12,
