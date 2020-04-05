51a20b0beb8a8f289fb15c32849de08be30c46a1
merlinoa <andy.merlino@tychobra.com>
2020-04-05 11:35:43 -0400

    admin panel db functions

--- R/dashboard_module.R
+++ R/dashboard_module.R
@@ -99,47 +99,11 @@ dashboard_module <- function(input, output, session) {
 
     if (is.null(.global_sessions$api_key)) {
       # find all sessions for this app
-      dat_sessions <- .global_sessions$conn %>%
-        dplyr::tbl(dbplyr::in_schema("polished", "sessions")) %>%
-        dplyr::filter(.data$app_uid == hold_app_uid) %>%
-        dplyr::select(.data$user_uid, .data$email, .data$is_active, .data$uid) %>%
-        collect()
-
-      app_sessions <- dat_sessions$uid
-
-      dat_actions <- .global_sessions$conn %>%
-        dplyr::tbl(dbplyr::in_schema("polished", "session_actions")) %>%
-        dplyr::filter(
-          .data$action == "activate",
-          .data$timestamp >= start_date,
-          .data$session_uid %in% app_sessions
-        ) %>%
-        dplyr::select(.data$session_uid, .data$timestamp) %>%
-        dplyr::collect() %>%
-        dplyr::mutate(date = as.Date(.data$timestamp, tz = "America/New_York"))
-
-      out <- dat_actions %>%
-        left_join(dat_sessions, by = c("session_uid" = "uid")) %>%
-        dplyr::group_by(.data$date, .data$user_uid) %>%
-        dplyr::summarize(n = dplyr::n()) %>%
-        dplyr::ungroup()
-
-      if (nrow(out) > 0) {
-        # make sure all days are included even if zero sessions in a day
-        first_day <- min(out$date)
-
-        all_days <- tibble::tibble(
-          date = seq.Date(
-            from = first_day,
-            to = lubridate::today(tzone = "America/New_York"),
-            by = "day"
-          )
-        )
-
-        out <- all_days %>%
-          dplyr::left_join(out, by = "date") %>%
-          mutate(n = ifelse(is.na(n), 0, n))
-      }
+      out <- get_daily_sessions(
+        .global_sessions$conn,
+        app_uid_ = hold_app_uid,
+        start_date = start_date,
+      )
     } else {
       res <- httr::GET(
         url = paste0(.global_sessions$hosted_url, "/daily-sessions"),
@@ -246,14 +210,11 @@ dashboard_module <- function(input, output, session) {
       hold_app_uid = .global_sessions$app_name
 
       if (is.null(.global_sessions$api_key)) {
-        out <- .global_sessions$conn %>%
-          dplyr::tbl(dbplyr::in_schema("polished", "sessions")) %>%
-          dplyr::filter(
-            .data$app_uid == hold_app_uid,
-            .data$is_active == TRUE
-          ) %>%
-          dplyr::distinct(.data$email) %>%
-          dplyr::collect()
+        out <- get_active_users(
+          .global_sessions$conn,
+          hold_app_uid
+        )
+
       } else {
         res <- httr::GET(
           url = paste0(.global_sessions$hosted_url, "/active-users"),
