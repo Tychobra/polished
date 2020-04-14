c38566cb234d133ef08390b187f5ffd3907dbd9a
phoward38 <patrick.howard@tychobra.com>
2020-04-14 14:10:42 -0400

    Added 'sign_in_no_invite_module_ui' to 'no_invited_required' example

--- inst/examples/no_invite_required/ui.R
+++ inst/examples/no_invite_required/ui.R
@@ -16,5 +16,7 @@ ui <- fluidPage(
 
 secure_ui(
   ui,
-  sign_in_page_ui = sign_in_no_invite_module_ui("sign_in")
+  sign_in_page_ui = sign_in_ui_default(
+    sign_in_no_invite_module_ui("sign_in")
+  )
 )
