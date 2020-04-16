19e1e41c191a6108d435f1761c2b377882209876
merlinoa <andy.merlino@tychobra.com>
2020-04-16 11:38:03 -0400

    RPostgres db connections automatically disconnected, so removed code to disconnect on app stop

--- inst/examples/polished_hosted_example/global.R
+++ inst/examples/polished_hosted_example/global.R
@@ -6,5 +6,6 @@ app_config <- config::get()
 
 global_sessions_config(
   app_name = app_config$app_name,
-  api_key = app_config$polished_key
+  api_key = app_config$polished_key,
+  api_version = "dev"
 )
