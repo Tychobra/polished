8af8c274d7ad6aafec4bfb4749e7e1f1d6e298fb
merlinoa <andy.merlino@tychobra.com>
2020-05-19 15:44:06 -0400

    commented out dev global_sessions_config for testing

--- inst/examples/polished_hosted_example/global.R
+++ inst/examples/polished_hosted_example/global.R
@@ -6,5 +6,11 @@ app_config <- config::get()
 
 global_sessions_config(
   app_name = "polished_hosted_example",
-  api_key = app_config$api_key
+  api_key = app_config$api_key_prod,
 )
+
+#global_sessions_config(
+#  app_name = "polished_hosted_example",
+#  api_key = app_config$api_key_dev,
+#  api_version = "dev"
+#)
