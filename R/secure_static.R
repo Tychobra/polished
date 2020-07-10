e0d07739216fc2d4c9699f277e45ba25cfc6d006
merlinoa <andy.merlino@tychobra.com>
2020-07-10 10:52:09 -0400

    added tryCatch around sign out API request

--- R/secure_static.R
+++ R/secure_static.R
@@ -75,9 +75,16 @@ secure_static <- function(html_file_path, global_sessions_config_args,
   )
 
   server <- secure_server(function(input, output, session) {
+
     observeEvent(input$sign_out, {
-      sign_out_from_shiny(session)
-      session$reload()
+
+      tryCatch({
+        sign_out_from_shiny(session)
+        session$reload()
+      }, error = function(err) {
+        print(err)
+      })
+
     })
   })
 
