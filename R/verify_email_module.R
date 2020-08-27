e023d7028ce3d79df8bbed35fe3a62c559b3af97
merlinoa <andy.merlino@tychobra.com>
2020-08-27 16:19:38 -0400

    email verification page - automatically check if the user's email has been verified every 5 seconds

--- R/verify_email_module.R
+++ R/verify_email_module.R
@@ -56,13 +56,75 @@ verify_email_module_ui <- function(id) {
 #'
 #' @importFrom shiny observeEvent
 #' @importFrom shinyFeedback showToast
+#' @importFrom httr GET POST content status_code
 #'
 #' @noRd
 #'
 verify_email_module <- function(input, output, session) {
 
 
+  ### check every 5 seconds if the user has verified their email address yet ---
 
+  # the number of times that the API has been queried
+  count_polls <- 0
+
+  user_from_db <- reactivePoll(
+    5000,
+    session,
+    checkFunc = function() {
+      runif(1)
+    },
+    valueFunc = function() {
+
+      count_polls <<- count_polls + 1
+
+      # only poll the API to continue checking if the user has verified their email
+      # address for 8.33 minutes.  After 8.33 minutes, the user will need to refresh their
+      # page after verifiying their email address.  This is to avoid users sitting on the
+      # verification page and continuously pinging the API for no reason.
+      if (count_polls < 100) {
+        tryCatch({
+
+          res <- httr::GET(
+            url = paste0(.global_sessions$hosted_url, "/users"),
+            httr::authenticate(
+              user = .global_sessions$api_key,
+              password = ""
+            ),
+            query = list(
+              user_uid = session$userData$user()$user_uid
+            )
+          )
+
+          res_content <- jsonlite::fromJSON(
+            httr::content(res, type = "text", encoding = "UTF-8")
+          )
+
+          if (identical(httr::status_code(res), 200L)) {
+            return(res_content)
+          } else {
+            stop(res_content, call. = FALSE)
+          }
+        }, error = function(err) {
+
+          print(err)
+
+        })
+      }
+
+    }
+  )
+
+  shiny::observeEvent(user_from_db(), {
+    hold_user <- user_from_db()
+
+    if (isTRUE(hold_user$email_verified)) {
+      session$reload()
+    }
+  })
+
+
+  ### handle resending of verification email
 
   shiny::observeEvent(input$resend_verification_email, {
 
@@ -84,7 +146,12 @@ verify_email_module <- function(input, output, session) {
         encode = "json"
       )
 
-      httr::stop_for_status(res)
+      if (!identicl(httr::status_code(res), 200L)) {
+        res_content <- jsonlite::fromJSON(
+          httr::content(res, type = "text", encoding = "UTF-8")
+        )
+        stop(res_content, call. = FALSE)
+      }
 
       shinyFeedback::showToast("success", paste0("Verification email send to ", hold_email))
     }, error = function(err) {
