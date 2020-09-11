dd9f2f07aabfe4d5eb47474ab2b1d33e5b425313
merlinoa <andy.merlino@tychobra.com>
2020-09-11 13:06:36 -0400

    added box-shadow to sign in panels

--- R/sign_in_ui_default.R
+++ R/sign_in_ui_default.R
@@ -83,6 +83,7 @@ sign_in_ui_default <- function(
           background-color: #fff;
           color: #080021;
           margin: 0 auto;
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
         }
 
         .auth_panel_2 {
@@ -92,6 +93,7 @@ sign_in_ui_default <- function(
           background-color: #fff;
           color: #080021;
           margin: 0 auto;
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
         }
 
         .btn-primary {
