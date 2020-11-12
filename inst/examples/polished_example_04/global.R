92436bb49e168dacd091b7d484343c001d2c3666
merlinoa <andy.merlino@tychobra.com>
2020-11-12 15:30:58 -0500

    comment

--- inst/examples/polished_example_04/global.R
+++ inst/examples/polished_example_04/global.R
@@ -9,5 +9,6 @@ app_config <- config::get()
 global_sessions_config(
   app_name = "polished_example_04",
   api_key = app_config$api_key,
+  # add sentry to polished.  This will log all JS error to Sentry.io.
   sentry_dsn = app_config$sentry_dsn
 )
