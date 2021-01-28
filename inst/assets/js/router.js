68ae9a6ba7e9e8778b49c802a5f0ecc9d25f2054
merlinoa <andy.merlino@tychobra.com>
2021-01-28 13:07:10 -0500

    keep alive

--- inst/assets/js/router.js
+++ inst/assets/js/router.js
@@ -3,4 +3,16 @@
 // force page reload after browser back and forward buttons are clicked
 window.onpopstate = function (event) {
   window.location.reload(true);
-};
+}; // ping server every 3 minutes to keep Shiny session from graying out as long as
+// browser tab is open for the app.
+
+
+$(function () {
+  function keepAlive() {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', "/polish/js/router.js");
+    httpRequest.send(null);
+  }
+
+  setInterval(keepAlive, 3 * 60 * 1000);
+});
