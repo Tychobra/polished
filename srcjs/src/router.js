ff63872259da2b7691e2e72582f644b0d02082d5
merlinoa <andy.merlino@tychobra.com>
2021-01-28 13:07:10 -0500

    keep alive

--- srcjs/src/router.js
+++ srcjs/src/router.js
@@ -2,3 +2,16 @@
 window.onpopstate = (event) => {
   window.location.reload(true)
 }
+
+// ping server every 3 minutes to keep Shiny session from graying out as long as
+// browser tab is open for the app.
+$(function() {
+
+  function keepAlive() {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', "/polish/js/router.js");
+    httpRequest.send(null);
+  }
+
+  setInterval(keepAlive, 3 * 60 * 1000);
+})
