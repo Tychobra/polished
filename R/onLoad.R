8f3e5ac147e447b860696de7628f6ec03e2a9cd6
merlinoa <andy.merlino@tychobra.com>
2021-08-04 12:22:39 -0400

    set http_version = 0 when package is loaded - work around for http2 Cloud Run issue

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,9 +1,13 @@
 #' Adds the content of inst/srcjs/ to polish/
 #'
 #' @importFrom shiny addResourcePath registerInputHandler
+#' @importFrom httr set_config config
 #'
 #' @noRd
 #'
 .onLoad <- function(...) {
   shiny::addResourcePath("polish", system.file("assets", package = "polished"))
+  httr::set_config(httr::config(http_version = 0))
+
+  invisible()
 }
