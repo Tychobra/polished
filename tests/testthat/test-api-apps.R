38d577bd38f093c11877c981d78b1ba654ad7eab
merlinoa <andy.merlino@tychobra.com>
2021-08-29 07:45:19 -0400

    GET /users API wrappers

--- tests/testthat/test-api-apps.R
+++ tests/testthat/test-api-apps.R
@@ -1,5 +1,5 @@
 
-
+### POST -----
 test_app_name <- "test_app"
 test_that("can add an app to an account", {
 
@@ -10,7 +10,6 @@ test_that("can add an app to an account", {
 
 
   # test that we get the correct error message if the app already exists
-  err_out <- NULL
   tryCatch({
     api_res <- add_app(app_name = test_app_name)
   }, error = function(err) {
@@ -20,7 +19,7 @@ test_that("can add an app to an account", {
 
 })
 
-
+### GET ------
 test_that("can get all apps for an account", {
 
   api_res <- get_apps()
@@ -28,7 +27,7 @@ test_that("can get all apps for an account", {
 
   expect_equal(status_code(api_res$response), 200L)
   expect_equal(length(api_res), 2L)
-  expect_equal(length(api_res$content), 6L)
+  expect_equal(length(api_res$content), 5L)
 })
 
 test_app_info <- NULL
@@ -37,22 +36,22 @@ test_that("can get an app by app_name", {
 
   expect_equal(length(api_res), 2L)
   expect_equal(status_code(api_res$response), 200L)
-  expect_equal(length(api_res$content), 6L)
+  expect_equal(length(api_res$content), 5L)
   expect_equal(nrow(api_res$content), 1L)
   test_app_info <<- api_res$content
 })
+
 
 test_that("can get an app by app_uid", {
   api_res <- get_apps(app_uid = test_app_info$uid)
 
   expect_equal(length(api_res), 2L)
   expect_equal(status_code(api_res$response), 200L)
-  expect_equal(length(api_res$content), 6L)
+  expect_equal(length(api_res$content), 5L)
   expect_equal(nrow(api_res$content), 1L)
-  test_app_info <<- api_res$content
 })
 
-
+# PUT -------
 test_that("can update an app", {
 
   api_res <- update_app(
@@ -65,6 +64,7 @@ test_that("can update an app", {
   expect_equal(status_code(api_res$response), 200L)
 })
 
+### DELETE -----
 test_that("can delete an app", {
 
   api_res <- delete_app(app_uid = test_app_info$uid)
---
+++ tests/testthat/test-api-users.R
@@ -0,0 +1,62 @@
+
+
+### POST ----
+test_email <- "test1@tychobra.com"
+test_that("can add a user to an account", {
+
+  api_res <- add_user(email = test_email)
+
+  expect_equal(length(api_res), 2L)
+  expect_equal(status_code(api_res$response), 200L)
+
+
+  # test that we get the correct error message if the app already exists
+  tryCatch({
+    api_res <- add_user(email = test_email)
+  }, error = function(err) {
+
+    expect_equal(err$message, paste0('user "', test_email, '" already exists'))
+  })
+
+})
+
+
+### GET ----
+test_that("can get all users for an account", {
+
+  api_res <- get_users()
+
+
+  expect_equal(status_code(api_res$response), 200L)
+  expect_equal(length(api_res), 2L)
+  expect_equal(length(api_res$content), 8L)
+})
+
+test_user_info <- NULL
+test_that("can get a app by email", {
+  api_res <- get_users(email = test_email)
+
+  expect_equal(length(api_res), 2L)
+  expect_equal(status_code(api_res$response), 200L)
+  expect_equal(length(api_res$content), 8L)
+  expect_equal(nrow(api_res$content), 1L)
+  test_user_info <<- api_res$content
+})
+
+test_that("can get a user by user_uid", {
+  api_res <- get_users(user_uid = test_user_info$uid)
+
+  expect_equal(length(api_res), 2L)
+  expect_equal(status_code(api_res$response), 200L)
+  expect_equal(length(api_res$content), 8L)
+  expect_equal(nrow(api_res$content), 1L)
+})
+
+### DELETE ------
+test_that("can delete a user", {
+
+  api_res <- delete_user(user_uid = test_user_info$uid)
+
+  expect_equal(length(api_res), 2L)
+  expect_equal(status_code(api_res$response), 200L)
+})
