a4288382ae5e23946263e942814b65cc5e02dd3b
merlinoa <andy.merlino@tychobra.com>
2021-09-09 16:40:52 -0400

    switched test to use dev API

--- tests/testthat/setup-api-options.R
+++ tests/testthat/setup-api-options.R
@@ -3,12 +3,12 @@ the_config <- config::get()
 
 polished::set_api_key(the_config$api_key)
 
-polished:::set_api_url(
-  api_url = "http://0.0.0.0:8080/v1",
-  host_api_url = "http://0.0.0.0:8081/v1"
-)
+#polished:::set_api_url(
+#  api_url = "http://0.0.0.0:8080/v1",
+#  host_api_url = "http://0.0.0.0:8081/v1"
+#)
 
-# polished:::set_api_url(
-#   api_url = "https://auth-api-dev.polished.tech/v1",
-#   host_api_url = "http://host-api-dev.polished.tech/v1"
-# )
+polished:::set_api_url(
+  api_url = "https://auth-api-dev.polished.tech/v1",
+  host_api_url = "http://host-api-dev.polished.tech/v1"
+)
