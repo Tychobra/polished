a25b0c72c7194ab68d0d62377e7c845faf2055b2
merlinoa <andy.merlino@tychobra.com>
2021-09-20 11:10:30 -0400

    removed "api_url" argument from example

--- inst/examples/polished_example_01/global.R
+++ inst/examples/polished_example_01/global.R
@@ -7,6 +7,5 @@ app_config <- config::get()
 # configure polished
 global_sessions_config(
   app_name = "polished_example_01",
-  api_key = app_config$api_key,
-  api_url = app_config$api_url
+  api_key = app_config$api_key
 )
