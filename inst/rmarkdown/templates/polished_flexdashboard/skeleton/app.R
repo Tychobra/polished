a04592e586fce056886c63da64119c16e5691128
Patrick Howard <patrick.howard@tychobra.com>
2022-01-21 09:58:30 -0500

    Fix Rmarkdown template comment typo

--- inst/rmarkdown/templates/polished_flexdashboard/skeleton/app.R
+++ inst/rmarkdown/templates/polished_flexdashboard/skeleton/app.R
@@ -3,9 +3,9 @@
 polished::secure_rmd(
   rmd_file_path = "<path to your .Rmd file>",
 
-  # if your Polished API key is not set as an R environment variable, then you
-  # can it here.
-  #polished_config_args = list(
+  # if your Polished API key is not set as an R environment variable (default: `POLISHED_API_KEY`),
+  # then you must set it below.
+  # polished_config_args = list(
   #  api_key = "<your Polished API key>"
-  #)
+  # )
 )
