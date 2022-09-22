7c1da17a0426abc54ce0346356a402e47bc1911a
merlinoa <andy.merlino@tychobra.com>
2022-09-22 16:57:01 -0400

    get app_ui from .polished

--- R/auth_filter.R
+++ R/auth_filter.R
@@ -8,54 +8,51 @@
 #'
 #' @export
 #'
-auth_filter <- function(app_uid = .polished$app_uid) {
+auth_filter <- function(req, res) {
 
-  function(req, res) {
+  err_msg <- NULL
+  tryCatch({
 
-    err_msg <- NULL
-    tryCatch({
-
-      # attempt to find session based on cookie
-      polished_cookie <- req$cookies$polished
-      if (is.null(polished_cookie)) {
-        res$status <- 401L # unauthorized
-        stop("polished cookie not provided", call. = FALSE)
-      }
-
-      hold_session <- get_sessions(
-        app_uid = app_uid,
-        hashed_cookie = polished_cookie
-      )
-
-      sc <- status_code(hold_session$response)
-      if (!identical(sc, 200L)) {
-        res$status_code <- sc
-        stop(sc$content$error, call. = FALSE)
-      }
-
-      if (is.null(hold_session$content)) {
-        res$status <- 401L
-        stop("session not found", call. = FALSE)
-      }
-
-      req$polished_session <- hold_session$content
-
-    }, error = function(err) {
-      print(err)
-
-      if (res$status == 200L) {
-        res$status <- 500L
-      }
-
-      err_msg <<- err$message
-    })
-
-    if (!is.null(err_msg)) {
-      return(list(
-        error = jsonlite::unbox(err_msg)
-      ))
-    } else {
-      plumber::forward()
+    # attempt to find session based on cookie
+    polished_cookie <- req$cookies$polished
+    if (is.null(polished_cookie)) {
+      res$status <- 401L # unauthorized
+      stop("polished cookie not provided", call. = FALSE)
     }
+
+    hold_session <- get_sessions(
+      app_uid = .polished$app_uid,
+      hashed_cookie = polished_cookie
+    )
+
+    sc <- status_code(hold_session$response)
+    if (!identical(sc, 200L)) {
+      res$status_code <- sc
+      stop(sc$content$error, call. = FALSE)
+    }
+
+    if (is.null(hold_session$content)) {
+      res$status <- 401L
+      stop("session not found", call. = FALSE)
+    }
+
+    req$polished_session <- hold_session$content
+
+  }, error = function(err) {
+    print(err)
+
+    if (res$status == 200L) {
+      res$status <- 500L
+    }
+
+    err_msg <<- err$message
+  })
+
+  if (!is.null(err_msg)) {
+    return(list(
+      error = jsonlite::unbox(err_msg)
+    ))
+  } else {
+    plumber::forward()
   }
 }
