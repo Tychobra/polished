2d629268edc44ac17f757ddee32332ba90c78429
merlinoa <andy.merlino@tychobra.com>
2023-01-24 10:09:03 -0500

    fixed test skip

--- tests/testthat.R
+++ tests/testthat.R
@@ -10,7 +10,7 @@ if (length(strsplit(packageDescription("polished")$Version, "\\.")[[1]]) > 3) {
   Sys.setenv("SKIP_POLISHED_TESTS"="true")
 }
 
-if (identical(Sys.getenv("SKIP_POLISHED_TESTS"), "true")) {
+if (!identical(Sys.getenv("SKIP_POLISHED_TESTS"), "true")) {
   test_check("polished")
   #testthat::test_local()
 }
